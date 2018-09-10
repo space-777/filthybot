74a12920d3923d3bc85f89a8493d72ab2d0242a2
manav2511 <manav2511@users.noreply.github.com>
2018-09-11 00:04:18 +0530

    add restart

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from osuapi import OsuApi, AHConnector
 import aiohttp
 import asyncio
+import os
+import sys
 
 
 TOKEN = 'NDg4NzMxMDE2NTY0MDQ3ODgz.DngeQQ.egVl3OgvvifBWBaNZeN9nR8QP1I'
@@ -10,6 +12,12 @@ TOKEN = 'NDg4NzMxMDE2NTY0MDQ3ODgz.DngeQQ.egVl3OgvvifBWBaNZeN9nR8QP1I'
 
 client = commands.Bot(command_prefix = '!f')
 
+def restart_program():
+    """Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
 
 
 @client.event
@@ -26,6 +34,11 @@ async def ping():
     
 @client.command()
 async def shutdown():
+    await client.close()
+    
+@client.command()
+async def restart()
+    restart_program()
     await client.close()
     
 
