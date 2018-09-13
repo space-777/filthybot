c76f38725bedee1a31ede1bfc5643e81ce974f9f
manav2511 <manav2511@users.noreply.github.com>
2018-09-13 18:31:34 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,7 +13,7 @@ api = OsuApi(apicode, connector=ReqConnector())
 def display(param):
 	profile=api.get_user(param)
 	print(profile)
-	tit="Osu Profile for "+param
+	tit="Osu Profile for "+profile[0].username
 	desc="Username: "+profile[0].username+"\n"+"PP: "+str(profile[0].pp_raw)+"\n"+"Rank: "+str(profile[0].pp_rank)+"\n"+"Playcount: "+str(profile[0].playcount)
 	em = discord.Embed(title= tit, description=desc, colour=0xDEADBF)
 	return em
@@ -24,15 +24,21 @@ def scoredisp(user,scores,amt):
 	count = 1
 	for i in scores:
 		bmp = api.get_beatmaps(beatmap_id=i.beatmap_id)
-		desc = desc + str(count) + ".) " + bmp[0].title + "[" + bmp[0].version + "]" + " -- PP : " + str(i.pp) + "\n" 
+		desc = desc + str(count) + ".) " + bmp[0].title + "[" + bmp[0].version + "] " + str(i.enabled_mods) + " -- PP : " + str(i.pp) +  " Played on " + str(i.date) + "\n"   
 		count+=1
 
 	em = discord.Embed(title= tit, description=desc, colour=0xDEADBF)
 	return em
 
+def recentdisp(user,scores,amt):
+	tit = "Recent "+str(amt)+" scores for "+user
+	desc=""
+	count = 1
+	for i in scores:
+		bmp = api.get_beatmaps(beatmap_id=i.beatmap_id)
+		desc = desc + str(count) + ".) " + bmp[0].title + "[" + bmp[0].version + "] " + str(i.enabled_mods) + " Played on " + str(i.date) + "\n" 
+		count+=1
 
+	em = discord.Embed(title= tit, description=desc, colour=0xDEADBF)
+	return em
 
-def ret50(user,val=50):
-	scores = api.get_user_best(user,limit=val)
-	#print(scores)
-	return scores
