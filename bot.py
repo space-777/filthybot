f1be685b72e3bafe8cdf0a04016b6f4a437b41b5
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2018-09-13 10:51:35 +0530

    nothing

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
+import os
+import sys
+import Location
 import discord
 from discord.ext import commands
 from osuapi import OsuApi, ReqConnector
 import requests
-import os
-import sys
 
 
-TOKEN = 'NDg4NzMxMDE2NTY0MDQ3ODgz.DngeQQ.egVl3OgvvifBWBaNZeN9nR8QP1I'
-apicode="92a4871f7a42d7015d58a9acf3dda2f662ba28db"
+TOKEN = 'NDg5NDU2MTc5MjQ5MDIwOTU5.DntehQ.IlU8WNcpOpO89bYTRnKhPGPsIM0'
+apicode = "92a4871f7a42d7015d58a9acf3dda2f662ba28db"
 api = OsuApi(apicode, connector=ReqConnector())
 
-client = commands.Bot(command_prefix = '!f')
+client = commands.Bot(command_prefix='!f')
+
 
 def restart_program():
     """Restarts the current program.
@@ -22,32 +24,33 @@ def restart_program():
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    ready_message = "Logged in as" + client.user.name + "\n ID:" +  client.user.id
+    print(ready_message)
 
 
 @client.command()
 async def ping():
     await client.say('pong')
-    
+
+
 @client.command()
 async def shutdown():
-    await client.close()
-    
+    await client.logout()
+
+
 @client.command()
 async def restart():
     restart_program()
     await client.close()
 
+
 @client.command(pass_context=True)
-async def osu(context,param):
-    profile=api.get_user(param)
-    tit="Osu Profile for "+param
-    desc="Username: "+profile[0].username+"\n"+"PP: "+str(profile[0].pp_raw)+"\n"+"Rank: "+str(profile[0].pp_rank)+"\n"+"Playcount: "+str(profile[0].playcount)
-    em = discord.Embed(title= tit, description=desc, colour=0xDEADBF)
+async def osu(context, param):
+    profile = api.get_user(param)  # fails if more than 1 param.
+    tit = "Osu Profile for "+param
+    desc = "Username: "+profile[0].username+"\n"+"PP: "+str(profile[0].pp_raw)+"\n"+"Rank: "+str(profile[0].pp_rank)+"\n"+"Playcount: "+str(profile[0].playcount)
+    em = discord.Embed(title=tit, description=desc, colour=0xDEADBF)
     await client.send_message(context.message.channel, embed=em)
-    
+
 
 client.run(TOKEN)
